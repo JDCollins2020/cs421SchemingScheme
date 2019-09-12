25f103328ababad5ebe940e893de52cf5f1fcd58
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2019-09-12 12:34:38 -0500

    implemented majority of TokenStream

--- SchemingWScheme/src/Driver.java
+++ SchemingWScheme/src/Driver.java
@@ -3,7 +3,12 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 
+		TokenStream tx = new TokenStream("hello.txt");
+		for(int line = 0; line < 8; line++) {
+			tx.nextLine();
+			tx.peek();
+		}
+		
 	}
 
 }
--- SchemingWScheme/src/TokenStream.java
+++ SchemingWScheme/src/TokenStream.java
@@ -1,4 +1,49 @@
+import java.io.*;
 
 public class TokenStream {
-
+	private BufferedReader fBuffer = null;
+	private String[] currentStream = null;
+	private int streamNdx = -1;
+	public TokenStream(String fileName) {
+		try {
+			fBuffer = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	//remove return, instead update current stream
+	//every nextline sets streamNdx to 0;
+	public void nextLine() {
+		String line = "";
+		streamNdx = 0;
+		try {
+			line = fBuffer.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(line == null) {
+			System.out.println("null");
+			currentStream = null;
+		}
+		else {
+			System.out.println(line);
+			currentStream = tokenize(line);
+		}
+	}
+	
+	private String[] tokenize(String line) {
+		String[] tokens = line.trim().split("\\s+");
+		return tokens;
+	}
+	
+	public void peek() {
+		if(currentStream != null) {
+			for(int ndx = 0; ndx < currentStream.length; ndx++) {
+				System.out.println("\""+currentStream[ndx]+ "\"");
+			}
+		}
+	}
+	
+	
+	
 }
